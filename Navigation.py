f351a1c0caa7ac0ce735866018b3946727a15fce
kpasad <kpasad@gmail.com>
2021-01-24 16:06:46 -0800

    Code comments and agent Gif added

--- Navigation.py
+++ Navigation.py
@@ -11,11 +11,17 @@ from agent import Agent
 
 import pickle as pk
 
+#Set up the parameters
 params=parameters()
 params.network = 'dqn'  # 'dqn, dueling_dqn'
 params.double_dqn = 'disable'  # enable, disable
-params.buffer = 'baseline'  # baseline,priority_replay
-params.op_filename_prefix ='dqn'
+params.buffer = 'baseline'  # baseline (other not supported yet)
+params.op_filename_prefix ='dummy'
+params.eps_start=1.0
+params.eps_end=0.01
+params.eps_decay=0.995
+params.n_episodes=2000
+
 
 env = UnityEnvironment(file_name="Banana_Windows_x86_64/Banana.exe",no_graphics=True)
 
@@ -36,33 +42,32 @@ print('States look like:', state)
 state_size = len(state)
 print('States have length:', state_size)
 
-seed=0
+seed=0 #Seed for epsilon greedy as well as the NN
 agent = Agent(state_size, action_size ,seed,params)
 
 
-params.eps_start=1.0
-params.eps_end=0.01
-params.eps_decay=0.995
+
 eps = params.eps_start  # initialize epsilon
-n_episodes=2000
+n_episodes=params.n_episodes
 scores=[]
-scores_window=deque(maxlen=100)
+scores_window=deque(maxlen=100) #Window for moving average.
+
 for i_episode in range(1,n_episodes+1):
     env_info = env.reset(train_mode=True)[brain_name] # reset the environment
     state = env_info.vector_observations[0]            # get the current state
     score = 0                                          # initialize the score
 
     while True:
-        action = agent.act(state,eps)
-        env_info = env.step(int(action))[brain_name]
-        next_state = env_info.vector_observations[0]
-        reward = env_info.rewards[0]
-        done = env_info.local_done[0]
-        agent.step(state, action, reward, next_state, done)
+        action = agent.act(state,eps) #Call the Agent to act on the current state
+        env_info = env.step(int(action))[brain_name] #Envrionment response to action.
+        next_state = env_info.vector_observations[0] #Next state transition
+        reward = env_info.rewards[0] #Reward earned when state->next_state
+        done = env_info.local_done[0] #Env signals if episode is done (boolean)
+        agent.step(state, action, reward, next_state, done) #Agent updates learning rule based on current state,action,rewards, next_state
 
-        score += reward
-        state = next_state
-        if done:
+        score += reward #Accumulate reward for this state
+        state = next_state #Move to next state.
+        if done: #If env is done with the current episode, then exit the episode.
             break
 
     scores_window.append(score)  # save most recent score
